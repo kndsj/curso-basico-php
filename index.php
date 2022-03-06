e672d328d0a618dbf688ea97063a044fb7f39699
kevin <kevinplus32@gmail.com>
2022-03-06 14:36:21 -0600

    clase 12

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
-	echo "hola"
+	$nombre = "kevin";
+	// CONCATENAR echo "hola $nombre";
+
+	$personas = [
+		'carlos' => 22
+	];
+
+	//Debugging
+	// var_dump($personas);
+	
+	//CONSTANTES
+	define('NOMBRES','jeremy');
+
+	//NUEVA
+	const hola = 'hola';
+	
+	
+
 ?>
 
